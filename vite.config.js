873aaaf6919fe6232745cae411d011e676d6aff7
alejandrodiazrivera <alejandrodiazrivera@outlook.com>
2025-02-02 20:02:32 +0100

    dd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/AguaTotalEntreprise/',
+  base: '/AguaTotalEntreprise/', // Must match your repo name exactly
   plugins: [react()],
   build: {
     outDir: 'dist'
