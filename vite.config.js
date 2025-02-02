f017574b169ec3196ee42b9678db368836c2acf1
alejandrodiazrivera <alejandrodiazrivera@outlook.com>
2025-02-02 17:47:06 +0100

    COPILTA

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    middlewareMode: 'html',
+    configureServer: (server) => {
+      server.middlewares.use((req, res, next) => {
+        if (res.statusCode === 404) {
+          res.statusCode = 200;
+          res.end('Custom 404 Page');
+        } else {
+          next();
+        }
+      });
+    }
+  }
 })
