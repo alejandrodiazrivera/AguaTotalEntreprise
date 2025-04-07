97bc3e1de8b1e445e8e9c1ab859f537d17707e57
alejandrodiazrivera <alejandrodiazrivera@outlook.com>
2025-04-07 17:30:54 +0200

    sizer

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/AguaTotalEntreprise/',
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets' // Ensures proper asset organization
-  }
 })
