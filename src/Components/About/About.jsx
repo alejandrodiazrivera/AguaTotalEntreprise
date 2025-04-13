5868cb51b1093164738b84a510adc14381a2e3cc
alejandrodiazrivera <alejandrodiazrivera@outlook.com>
2025-04-13 21:19:25 +0200

    totalinterprise

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -13,7 +13,7 @@ const About = ({ setPlayState }) => {
         <div className='about-right'>
             <h3>Om AGUA TOTALINTERPRISE</h3>
             <h2>Din pålidelige partner til VVS, murer- og tømrerarbejde</h2>
-            <p>Hos Agua Total Entreprise er vi din totalleverandør, når det gælder kvalitetsarbejde til hjem og erhverv. Vi kombinerer ekspertise inden for VVS, murer- og tømrerfaget for at sikre, at dine projekter bliver løst hurtigt, effektivt og med den højeste faglige standard.</p>
+            <p>Hos Agua Totalinterpriseer vi din totalleverandør, når det gælder kvalitetsarbejde til hjem og erhverv. Vi kombinerer ekspertise inden for VVS, murer- og tømrerfaget for at sikre, at dine projekter bliver løst hurtigt, effektivt og med den højeste faglige standard.</p>
             <p>Vores team af dygtige håndværkere arbejder tæt sammen for at levere helhedsløsninger, der både løser dine udfordringer og skaber værdi i længden. Uanset om du har brug for reparation af vandskader, ny flisebelægning eller en terrasse til din have, står vi klar til at hjælpe – altid med fokus på kvalitet, æstetik og pålidelighed.</p>
             <p>Vi ved, at bygge- og reparationsprojekter kan være en stor beslutning, og derfor prioriterer vi gennemsigtighed, ansvarlighed og et tæt samarbejde med dig som kunde. Vores kerneværdier er rodfæstet i troen på, at godt håndværk skaber tillid – og vi gør det rigtigt første gang.</p>
             <h2>Hvorfor vælge os?</h2>
